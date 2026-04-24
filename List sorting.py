0e9abbe3712d21797c46b692703083e12e0841ce
harshmonga56111-byte <harshmonga56111@gmail.com>
2026-04-24 11:06:35 +0530

    Create List sorting.py
    SORTING OF A LIST.

---
+++ List sorting.py
@@ -0,0 +1,13 @@
+
+
+l1 = ['Harsh','Aman' , 'Kartik', 'Prabhat']
+l1.sort()
+print(l1)
+
+l2=[100,21,50,36,1,320,755,95,21]
+l2.sort()
+print(l2)
+
+l3=[100,21,50,36,1,320,755,95,21]
+l3.sort(reverse = True)
+print(l3)
