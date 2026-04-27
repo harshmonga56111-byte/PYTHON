ba353779892df2ed0ec72179888e68abe75336b0
harshmonga56111-byte <harshmonga56111@gmail.com>
2026-04-27 22:47:34 +0530

    Create Dictionary.py
    1. Created a dictionary named car.
    2. print that dictionary.
    3. printed the value of a particular segment .
    4. printed all the keys of an dictionaries.
    5. printed all the values of an dictionary.

---
+++ Dictionary.py
@@ -0,0 +1,18 @@
+car = {
+  "brand": "Toyota",
+  "model": "Forturner",
+  "year": 2018
+}
+print(car)
+
+#printing the value a key 
+print(car['brand'])
+
+#printing the keys of an dictionary
+x = car.keys()
+print(x)
+
+#printing the values of an dictionary
+y=car.values()
+print(y)
+
