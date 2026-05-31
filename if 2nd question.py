44e2a0a1209f8ba6226be8df2e2ab2c6b3c7212a
harshmonga56111-byte <harshmonga56111@gmail.com>
2026-05-31 23:44:35 +0530

    Create if 2nd question.py
    Checking if the candidate is eligible for voting .

---
+++ if 2nd question.py
@@ -0,0 +1,8 @@
+#Checking if the candidate is eligible for voting .
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+else:
+  print("You can't vote")
